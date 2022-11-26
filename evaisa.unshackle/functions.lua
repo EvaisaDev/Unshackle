8ebfd3f7f79f90404fb9ae5a65f485af8fe8a3cb
Evaisa <evagiacosa1@gmail.com>
2022-11-26 10:55:49 +0100

    First commit

---
+++ evaisa.unshackle/functions.lua
@@ -0,0 +1,25 @@
+local smallfolk = dofile("mods/evaisa.difficulty/lib/smallfolk.lua")
+
+local custom_api = {}
+
+local api = {}
+
+function RegisterFunction(name, func)
+    table.insert(custom_api, {name = name, func = func})
+    api[name] = function(...)
+        local arg={...}
+        local saved = smallfolk.dumpsies(arg);
+        ModSettingSet("unshackle."..name, saved)
+    end
+end
+
+function Update()
+    for i, v in ipairs(custom_api)do
+        if(ModSettingGet("unshackle."..v.name) ~= nil)then
+            v.func(unpack(smallfolk.loadsies(ModSettingGet("unshackle."..v.name))))
+            ModSettingRemove("unshackle."..v.name)
+        end
+    end
+end
+
+return api
---
+++ evaisa.unshackle/init.lua
@@ -0,0 +1,23 @@
+for i = 0, ModSettingGetCount()-1 do
+    local setting = ModSettingGetAtIndex( i )
+    -- if setting starts with unshackle. then remove it
+    if(setting ~= nil)then
+        if string.sub(setting, 1, 10) == "unshackle." then
+            ModSettingRemove(setting)
+        end
+    end
+end
+
+function OnMagicNumbersAndWorldSeedInitialized()
+    ModLuaFileAppend("mods/evaisa.unshackle/functions.lua", "mods/evaisa.unshackle/example.lua")
+end
+
+function OnWorldPreUpdate() 
+    dofile("mods/evaisa.unshackle/functions.lua")
+    Update()
+end
+
+function OnPlayerSpawned()
+    --local api = dofile("mods/evaisa.unshackle/functions.lua")
+    --api.Save("test.txt", "Hello world!")
+end
