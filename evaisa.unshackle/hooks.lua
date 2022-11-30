f36d637fd34f291ec4b02d96e3bbf98387395f93
Evaisa <evagiacosa1@gmail.com>
2022-11-30 23:43:13 +0100

    add pollnet

---
+++ evaisa.unshackle/hooks.lua
@@ -0,0 +1,9 @@
+hooks = {
+    OnWorldInitialized = function() end,
+    OnWorldPreUpdate = function() end,
+    OnWorldPostUpdate = function() end,
+    OnMagicNumbersAndWorldSeedInitialized = function() end,
+    OnPlayerSpawned = function() end,
+    OnPlayerDeath = function() end,
+}
+return hooks
--- evaisa.unshackle/init.lua
+++ evaisa.unshackle/init.lua
@@ -1,6 +1,28 @@
+package.path = package.path .. ";./mods/evaisa.unshackle/lib/?.lua"
+package.path = package.path .. ";./mods/evaisa.unshackle/lib/?/init.lua"
+package.cpath = package.cpath .. ";./mods/evaisa.unshackle/bin/?.dll"
+
 lfs = dofile("mods/evaisa.unshackle/lib/lfs.lua")
 
 dofile("data/scripts/lib/coroutines.lua")
+
+local function load(modulename)
+    local errmsg = ""
+    for path in string.gmatch(package.path, "([^;]+)") do
+      local filename = string.gsub(path, "%?", modulename)
+      local file = io.open(filename, "rb")
+      if file then
+        -- Compile and return the module
+        return assert(loadstring(assert(file:read("*a")), filename))
+      end
+      errmsg = errmsg.."\n\tno file '"..filename.."' (checked with custom loader)"
+    end
+    return errmsg
+end
+
+table.insert(package.loaders, 2, load)
+
+
 
 function isFile(name)
     if type(name)~="string" then return false end
@@ -36,14 +58,31 @@ for i = 0, ModSettingGetCount()-1 do
 end
 
 function OnMagicNumbersAndWorldSeedInitialized()
+    __loaded["mods/evaisa.unshackle/hooks.lua"] = nil
+    local hooks = dofile("mods/evaisa.unshackle/hooks.lua")
+    hooks.OnMagicNumbersAndWorldSeedInitialized()
     ModLuaFileAppend("mods/evaisa.unshackle/functions.lua", "mods/evaisa.unshackle/example.lua")
 end
 
+function OnWorldInitialized()
+    __loaded["mods/evaisa.unshackle/hooks.lua"] = nil
+    local hooks = dofile("mods/evaisa.unshackle/hooks.lua")
+    hooks.OnWorldInitialized()
+end
 
 function OnWorldPreUpdate() 
+    __loaded["mods/evaisa.unshackle/hooks.lua"] = nil
+    local hooks = dofile("mods/evaisa.unshackle/hooks.lua")
+    hooks.OnWorldPreUpdate()
     dofile("mods/evaisa.unshackle/functions.lua")
     wake_up_waiting_threads(1)
     Update()
+end
+
+function OnWorldPostUpdate()
+    __loaded["mods/evaisa.unshackle/hooks.lua"] = nil
+    local hooks = dofile("mods/evaisa.unshackle/hooks.lua")
+    hooks.OnWorldPostUpdate()
 end
 
 function OnPausePreUpdate()
@@ -52,6 +91,15 @@ function OnPausePreUpdate()
 end
 
 function OnPlayerSpawned()
+    __loaded["mods/evaisa.unshackle/hooks.lua"] = nil
+    local hooks = dofile("mods/evaisa.unshackle/hooks.lua")
+    hooks.OnPlayerSpawned()
     --local api = dofile("mods/evaisa.unshackle/functions.lua")
     --api.Save("test.txt", "Hello world!")
+end
+
+function OnPlayerDeath()
+    __loaded["mods/evaisa.unshackle/hooks.lua"] = nil
+    local hooks = dofile("mods/evaisa.unshackle/hooks.lua")
+    hooks.OnPlayerDeath()
 end
