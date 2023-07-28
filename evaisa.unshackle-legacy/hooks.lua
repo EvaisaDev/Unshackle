6f63c6a96373ddb25018f59ae2d827ab572d943d
Evaisa <evagiacosa1@gmail.com>
2023-07-28 17:08:17 +0200

    remove legacy unshackle files

--- evaisa.unshackle-legacy/hooks.lua
+++
@@ -1,9 +0,0 @@
-hooks = {
-    OnWorldInitialized = function() end,
-    OnWorldPreUpdate = function() end,
-    OnWorldPostUpdate = function() end,
-    OnMagicNumbersAndWorldSeedInitialized = function() end,
-    OnPlayerSpawned = function() end,
-    OnPlayerDeath = function() end,
-}
-return hooks
